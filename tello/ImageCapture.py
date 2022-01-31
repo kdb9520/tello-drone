8e82773dc033eb40aca9a79568031cc72e2d0aeb
Kellen Bell <kdb9520@rit.edu>
2022-01-31 18:38:48 -0500

    Created functions for ImageCapture.py.

--- tello/ImageCapture.py
+++ tello/ImageCapture.py
@@ -1,14 +1,27 @@
 from djitellopy import tello
 import cv2
 
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
 
-me.streamon()
+def main():
+    me = connect()
+    camera_on(me)
 
-while True:
-    img = me.get_frame_read().frame
-    img = cv2.resize(img, (360, 240))
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+def connect():
+    me = tello.Tello()
+    me.connect()
+    print(me.get_battery())
+    return me
+
+
+def camera_on(me):
+    me.streamon()
+    while True:
+        img = me.get_frame_read().frame
+        img = cv2.resize(img, (360, 240))
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
