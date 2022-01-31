7507af2c16f6daaf0c9c81c360c5eec73435cbef
Kellen Bell <kdb9520@rit.edu>
2022-01-31 08:05:27 -0500

    Cleaned up Mapping.py

--- tello/Mapping.py
+++ tello/Mapping.py
@@ -5,32 +5,44 @@ import cv2
 import math
 from time import sleep
 
-# PARAMETERS
-fSpeed = 11.7  # forward speed 11.7 cm/s
-aSpeed = 36  # angular speed in degree/s
-interval = 0.25
-
-dInterval = fSpeed * interval
-aInterval = aSpeed * interval
-
-x, y = 500, 500
-a = 0
-yaw = 0
-
-kp.init()
-
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
-
-points = [(0, 0), (0, 0)]
+# Constants
+FSPEED = 11.7  # forward speed 11.7 cm/s
+ASPEED = 36  # angular speed in degree/s
+INTERVAL = 0.25
 
 
-def getKeyboardInput():
+def main(FSPEED=11.7, ASPEED=36, INTERVAL=0.25):
+    dInterval, aInterval, x, y, a, yaw = init_values(FSPEED, ASPEED, INTERVAL)
+    points, me = init_drone()
+    run_loop(dInterval, aInterval, x, y, a, yaw, me, points)
+
+
+
+def init_values(FSPEED, ASPEED, INTERVAL):
+    dInterval = FSPEED * INTERVAL
+    aInterval = ASPEED * INTERVAL
+    x, y = 500, 500
+    a = 0
+    yaw = 0
+    return dInterval, aInterval, x, y, a, yaw
+
+
+def init_drone():
+    kp.init()
+
+    me = tello.Tello()
+    me.connect()
+    print(me.get_battery())
+
+    points = [(0, 0), (0, 0)]
+
+    return points, me
+
+
+def getKeyboardInput(dInterval, aInterval, x, y, a, yaw, me):
     lr, fb, ud, yv = 0, 0, 0, 0
     speed = 15
     angSpeed = 50
-    global x, y, yaw, a
     d = 0
 
     if kp.getKey("LEFT"):
@@ -69,7 +81,7 @@ def getKeyboardInput():
     if kp.getKey("e"):
         me.takeoff()
 
-    sleep(interval)
+    sleep(INTERVAL)
     a += yaw
     x += int(d * math.cos(math.radians(a)))
     y += int(d * math.sin(math.radians(a)))
@@ -86,15 +98,19 @@ def drawPoints(img, points):
                 (points[-1][0] + 10, points[-1][1] + 30), cv2.FONT_HERSHEY_PLAIN, 1,
                 (255, 0, 255), 1)
 
+def run_loop(dInterval, aInterval, x, y, a, yaw, me, points):
+    while True:
+        vals = getKeyboardInput(dInterval, aInterval, x, y, a, yaw, me)
+        me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
-while True:
-    vals = getKeyboardInput()
-    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+        img = np.zeros((1000, 1000, 3), np.uint8)
+        if points[-1][0] != vals[4] or points[-1][1] != vals[5]:
+            points.append((vals[4], vals[5]))
 
-    img = np.zeros((1000, 1000, 3), np.uint8)
-    if points[-1][0] != vals[4] or points[-1][1] != vals[5]:
-        points.append((vals[4], vals[5]))
+        drawPoints(img, points)
+        cv2.imshow("Output", img)
+        cv2.waitKey(1)
 
-    drawPoints(img, points)
-    cv2.imshow("Output", img)
-    cv2.waitKey(1)
+
+if __name__ == '__main__':
+    main()
