8b4cac4beb138dab6a99a84c4b33cc4eae528cb3
Kellen Bell <kdb9520@rit.edu>
2022-01-31 18:45:19 -0500

    Reformated KeyPressModule.py.

--- tello/KeyPressModule.py
+++ tello/KeyPressModule.py
@@ -3,29 +3,29 @@ import pygame
 
 def init():
     pygame.init()
-    win = pygame.display.set_mode((400, 400))
+    pygame.display.set_mode((400, 400))
 
 
 def getKey(keyName):
     ans = False
-    for eve in pygame.event.get(): pass
-    keyInput = pygame.key.get_pressed()
-    myKey = getattr(pygame, 'K_{}'.format(keyName))
-    if keyInput[myKey]:
+    for event in pygame.event.get():
+        pass
+    key_input = pygame.key.get_pressed()
+    my_key = getattr(pygame, 'K_{}'.format(keyName))
+    if key_input[my_key]:
         ans = True
     pygame.display.update()
-
     return ans
 
 
 def main():
-    if getKey("LEFT"):
-        print("Left Key Pressed")
-    if getKey("RIGHT"):
-        print("Right Key Pressed")
+    while True:
+        if getKey("LEFT"):
+            print("Left Key Pressed")
+        if getKey("RIGHT"):
+            print("Right Key Pressed")
 
 
 if __name__ == '__main__':
     init()
-    while True:
-        main()
+    main()
